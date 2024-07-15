0c170a3ae378f7774e544631ba5b65ce0431332e
Balmukund Kumar <bkumarmukund@gmail.com>
2024-07-15 21:13:59 +0530

    added dependency for jakarta servelt and mysql connector in pom.xml

--- src/main/java/com/bk/code/servlet/LoginServlet.java
+++ src/main/java/com/bk/code/servlet/LoginServlet.java
@@ -5,4 +5,33 @@ package com.bk.code.servlet;
  */
 public class LoginServlet {
     
+    
+    
+    /* 
+        private static Connection connection;
+        public static void main(String[] args) throws SQLException {
+        System.out.println("hello world");
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/testFirst";
+        String dbUser = "root";
+        String dbPass = "pa55bird";
+        String readQuery = "select * from students";
+
+        connection = DriverManager.getConnection(jdbcUrl,dbUser,dbPass);
+
+        PreparedStatement pstmt = connection.prepareStatement(readQuery);
+
+        ResultSet result = pstmt.executeQuery();
+
+        while (result.next()) {
+            System.err.print(result.getString("name"));
+            System.err.print("-----");
+            System.err.print(result.getString("email"));
+            System.out.println();
+        }
+
+        pstmt.close();
+        connection.close();
+
+    } */
 }
